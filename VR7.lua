ba4b435a21fa47e3f15938405edb990e613601e1
Majeedl12 <132606472+Majeedl12@users.noreply.github.com>
2023-09-20 11:24:06 +0300

    Update VR7.lua

--- VR7.lua
+++ VR7.lua
@@ -1,61 +1,66 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMael7/NewIceHub/main/Brookhaven"))()
---loadingpage
-local txt1 = game:GetService("CoreGui").IceHubLauncher.MainFrame.StartFrame.WelcomeFrame.IceHubText
-local img1 = game:GetService("CoreGui").IceHubLauncher.MainFrame.StartFrame.WelcomeFrame.IceHubImage
---arceus x page
-local txt2 = game:GetService("CoreGui").IceHubLauncher.MainFrame.StartFrame.ArceusXFrame.ArceusXText
-local image2 = game:GetService("CoreGui").IceHubLauncher.MainFrame.StartFrame.ArceusXFrame.ArceusXImage
-txt1.Text = "ط§ظ„ط¹ط±ط¨ ظ‡ظˆط¨ ط§ظٹط³ ط¨ظƒ ط§ظ‡ظ„ط§"
-txt1.TextSize = 18
-txt2.Text = " VR7 & ظ†ط§ط´ط±ظٹظ† ط§ظ„ط³ظƒط±ط¨طھ ظ…ط§ط¬ط¯ "
-txt1.TextSize = 18
-img1.Image = "rbxassetid://14815284566"
-image2.Image = "rbxassetid://14815284566"
-wait(8)
+local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/aaaa"))()
 
---maintexts
-wait(3)
-local brookhavnen = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame.Brookhaven
-brookhavnen.Text = "ط§ظ„ط¨ظٹظˆطھ ظ…ط§ط¨"
-brookhavnen.TextSize = 18
-local adoptme = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame["Adopt Me"]
-adoptme.Text = "ظ…ظٹ ط§ط¯ظˆط¨طھ"
-adoptme.TextSize = 18
-local simtolife = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame.SimToLife
-simtolife.Text = "ط³ظٹظ…طھظˆظ„ظٹظپ"
-simtolife.TextSize = 18
-local redcliff = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame.Redcliff
-redcliff.Text = "ط±ظٹط¯ظƒظ„ظٹظپ"
-redcliff.TextSize = 18
-local barry = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame["Berry Avenue"]
-barry.Text = "ط§ظپظٹظ†ظٹظˆ ط¨ظٹط±ظٹ"
-barry.TextSize = 18
-wait(2)
-local publicbath = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame["Public Bathroom"]
-publicbath.Text = "ط§ظ„ط¹ط§ظ…ظ‡ ط§ظ„ظ…ظٹط§ظ‡ ط¯ظˆط±ط§طھ"
-publicbath.TextSize = 18
-local legendofspeed = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame["Legend Of Speed"]
-legendofspeed.Text = "ط§ظ„ط³ط±ط¹ظ‡ ط§ط³ط·ظˆط±ظ‡"
-legendofspeed.TextSize = 18
-local mm2 = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame["Murder vs Sheriff"]
-mm2.Text = "طھظˆ ط§ظ… ط§ظ…"
-mm2.TextSize = 18
-local animalsimu = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame["Animal Simulator"]
-animalsimu.Text = "ط§ظ„ط­ظٹظˆط§ظ†ط§طھ ظ…ط­ط§ظƒظٹ"
-animalsimu.TextSize = 18
-local animalsim = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame["Animal Sim PVP"]
-animalsim.Text = "pvp ط§ظ„ط­ظٹظˆط§ظ†ط§طھ ظ…ط­ط§ظƒظٹ"
-animalsim.TextSize = 15
-local animaluw = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame["Animal Sim UW"]
-animaluw.Text = "uw ط§ظ„ط­ظٹظˆط§ظ†ط§طھ ظ…ط­ط§ظƒظٹ"
-animaluw.TextSize = 15
-local flagswars = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame["Flag Wars"]
-flagswars.Text = "ط§ظ„ط§ط¹ظ„ط§ظ… ط­ط±ط¨"
-flagswars.TextSize = 18
-local bayview = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame.BayView
-bayview.Text = "ظپظٹظˆ ط¨ط§ظٹ"
-bayview.TextSize = 18
-local prisonlife = game:GetService("CoreGui").IceHubLauncher.MainFrame.GameContainer.ListedGame.ScrollingFrame["Prison Life"]
-prisonlife.Text = "ط§ظ„ط³ط¬ظ† ط­ظٹط§ط©"
-prisonlife.TextSize = 18
-loadstring(game:HttpGet("https://raw.githubusercontent.com/anyahubs/FE/main/button.lua"))()
+local UI = GUI:CreateWindow("VR7Hubs","VR7 ON TOP")
+
+local Home = UI:addPage("Main",1,true,6)
+
+
+Home:addToggle("تجربه",function(value)
+    
+end)
+
+
+Home:addButton("قتل من بعيد",function()
+    game.StarterGui:SetCore("SendNotification",{
+        Title = "تم تفعيل قتل من بعيد";
+        Text = "by VR7";
+    })
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Majeedl12/ScriptsByM7/main/Reach%20sword_fe.lua'))()
+end)
+
+Home:addTextBox("زياده الدم | الحد ١٠٠","Number here",function(value)
+    game.Players.LocalPlayer.Character.Humanoid.Health = value
+end)
+
+-- Just an example of how you would actually use it i guess
+
+local LP = UI:addPage("Other",1,true,6)
+
+-- Label
+
+LP:addLabel("سكربت بسيط وحلو","استمتع")
+
+
+LP:addLabel("استمتع","قائمه اللاعب")
+LP:addSlider("السرعه",16,150,function(value)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+end)
+LP:addTextBox("النط","Number here",function(value)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
+end)
+
+LP:addButton("قتل نفسك",function()
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+-- Dropdown 
+
+local PLIST = {}
+
+for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+    table.insert(PLIST,v.DisplayName)
+end
+
+LP:addDropdown("تنقل الى اللاعب",PLIST,4,function(value)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[value].Character.HumanoidRootPart.CFrame * CFrame.new(0,2,1)
+end)
+local Player = UI:addPage("الحقوق",1,true,6)
+Player:addLabel("by VR7","by MAJED  ")
+Player:addButton("نسخ رابط سيرفر الديسكورد",function()
+    setclipboard("https://discord.gg/kpJEGRdd")
+            print("link copied to clipoard")
+end)
+Player:addButton("نسخ اسم قناه اليوتيوب",function()
+        setclipboard("VR7")
+            print("link copied to clipoard")
+end)
